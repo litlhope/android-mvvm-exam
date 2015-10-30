74f7c5d98fba8052e8e52ead3d1f0c7e633d7a6b
Seong-il We <litlhope@gmail.com>
2015-10-30 16:49:42 +0900

    기본기능 구현 완료 1. setContentView를 DataBindingUtil을 이용하여 처리해야 한다. 2. 바인딩 클래스(여기서는 ActivityMainBinding)은 자동 생성된다.   - layout.xml 파일명에 맞춰서 생성된다.(이부분에서 한참 헤멤~ ㅠㅠ)   - 이 예제의 경우 layout.xml 파일명이 activity_main 이므로 ActivityMain 뒤에 Binding을 붙여서 ActivityMainBinding 이라는 클래스명으로 자동 생성됨. 3. 바인딩된 bean(예제에서는 User)의 값을 변경(set) 후 notifyChange() 메소드 호출만으로 화면에 표시되는 값이 변경됨(setUser  메소드 참조) 4. 바인딩된 레이아웃의 View에 접근하는 방법은 해당 아이디를 변수로 하여 Binding 클래스에 자동 생성된 변수를 사용(showUserDlg 메소드 앞 3라인 참조)

--- app/src/main/java/kr/heja/exam/android/mvvm/MainActivity.java
+++ app/src/main/java/kr/heja/exam/android/mvvm/MainActivity.java
@@ -1,13 +1,60 @@
 package kr.heja.exam.android.mvvm;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import kr.heja.exam.android.mvvm.BR;
+import kr.heja.exam.android.mvvm.data.User;
+import kr.heja.exam.android.mvvm.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+	private ActivityMainBinding mBinding;
+	private User mUser;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+		mUser = new User("FirstName", "LastName");
+		mBinding.setUser(mUser);
+	}
+
+	private void setUser() {
+		mUser.setFirstName("FTest");
+		mUser.setLastName("LTest");
+		mUser.notifyChange();
+	}
+
+	private void showUserDlg() {
+		mUser.setFirstName(mBinding.etFirstName.getText().toString());
+		mUser.setLastName(mBinding.etLastName.getText().toString());
+		mUser.notifyChange();
+
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+		alert.setTitle("사용자 정보")
+				.setMessage("firstName: " + mUser.getFirstName() + " / lastName" + mUser.getLastName())
+				.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.dismiss();
+					}
+				})
+				.show();
+	}
+
+	public void onButtonClick(View v) {
+		switch (v.getId()) {
+			case R.id.btnSetUser:
+				setUser();
+				break;
+
+			case R.id.btnShowUserDlg:
+				showUserDlg();
+				break;
+		}
 	}
 }
